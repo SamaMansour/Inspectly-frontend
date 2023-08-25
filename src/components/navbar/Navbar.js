554c00563344f05c93db506f7d4624b256fa3994
Sama Mansour <samamansour306@gmail.com>
2023-08-25 11:51:26 +0300

    Add login modal

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,28 @@
-import { Box, Flex, Link, Spacer, Button } from "@chakra-ui/react";
+import React, { useState } from "react";
+import {
+  Box,
+  Flex,
+  Link,
+  Spacer,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Input,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onOpen = () => setIsOpen(true);
+  const onClose = () => setIsOpen(false);
+
   return (
     <Box bg="teal.400" px={4} py={2} color="white">
       <Flex alignItems="center">
@@ -14,10 +36,36 @@ function Navbar() {
         <Link href="/contact" mx={2}>
           Contact
         </Link>
-        <Button colorScheme="teal" variant="ghost" mx={2}>
+        <Button colorScheme="teal" variant="ghost" mx={2} onClick={onOpen}>
           Login
         </Button>
       </Flex>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Login</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl mt={4}>
+              <FormLabel>Username</FormLabel>
+              <Input placeholder="Username" />
+            </FormControl>
+            <FormControl mt={4}>
+              <FormLabel>Password</FormLabel>
+              <Input placeholder="Password" type="password" />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Login
+            </Button>
+            <Button variant="ghost" onClick={onClose}>
+              Cancel
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }

fc40dfdac2cd4f20ec2c2f3ce73aecf7c987c8cd
Sama Mansour <samamansour306@gmail.com>
2023-08-26 18:58:41 +0300

    Fix close modal

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -16,16 +16,59 @@ import {
   FormControl,
   FormLabel,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useFormik } from "formik";
+
 
 function Navbar() {
   const [isLoginOpen, setLoginOpen] = useState(false);
   const [isSignupOpen, setSignupOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null); // to store the logged-in user
 
   const openLogin = () => setLoginOpen(true);
   const closeLogin = () => setLoginOpen(false);
 
   const openSignup = () => setSignupOpen(true);
   const closeSignup = () => setSignupOpen(false);
+
+  function handleLogin(username, password) {
+   axios
+     .post("http://localhost:8000/login/", { username, password })
+     .then((response) => {
+       if (response.data.status === "Login successful!") {
+         setCurrentUser(username);
+       }
+      setLoginOpen(false);
+     });
+  }
+
+  function handleSignup(username, email, password) {
+    axios
+      .post("http://localhost:8000/signup/", { username, password })
+      .then((response) => {
+        setSignupOpen(false);
+      });
+  }
+
+  const loginFormik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      handleLogin(values.username, values.password);
+    },
+  });
+
+  const signupFormik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      handleSignup(values.username, values.email, values.password);
+    },
+  });
 
   return (
     <Box bg="teal.400" px={4} py={2} color="white">
@@ -40,12 +83,21 @@ function Navbar() {
         <Link href="/contact" mx={2}>
           Contact
         </Link>
+        {currentUser ? (
+          <>
+            <Box>Welcome, {currentUser}</Box>
+            {/* You can also add a "Logout" button to handle user logout */}
+          </>
+        ) : (
+        <>
         <Button colorScheme="teal" variant="ghost" mx={2} onClick={openLogin}>
           Login
         </Button>
         <Button colorScheme="teal" variant="ghost" mx={2} onClick={openSignup}>
           Signup
         </Button>
+        </>
+        )}
       </Flex>
 
       {/* Login Modal */}
@@ -54,24 +106,37 @@ function Navbar() {
         <ModalContent>
           <ModalHeader>Login</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl mt={4}>
-              <FormLabel>Username</FormLabel>
-              <Input placeholder="Username" />
-            </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Password</FormLabel>
-              <Input placeholder="Password" type="password" />
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={closeLogin}>
-              Login
-            </Button>
-            <Button variant="ghost" onClick={closeLogin}>
-              Cancel
-            </Button>
-          </ModalFooter>
+          <form onSubmit={loginFormik.handleSubmit}>
+            <ModalBody pb={6}>
+              <FormControl mt={4}>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  placeholder="Username"
+                  name="username"
+                  onChange={loginFormik.handleChange}
+                  value={loginFormik.values.username}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  onChange={loginFormik.handleChange}
+                  value={loginFormik.values.password}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Login
+              </Button>
+              <Button variant="ghost" onClick={closeLogin}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
 
@@ -81,28 +146,37 @@ function Navbar() {
         <ModalContent>
           <ModalHeader>Signup</ModalHeader>
           <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl mt={4}>
-              <FormLabel>Username</FormLabel>
-              <Input placeholder="Username" />
-            </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Email</FormLabel>
-              <Input placeholder="Email" type="email" />
-            </FormControl>
-            <FormControl mt={4}>
-              <FormLabel>Password</FormLabel>
-              <Input placeholder="Password" type="password" />
-            </FormControl>
-          </ModalBody>
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={closeSignup}>
-              Signup
-            </Button>
-            <Button variant="ghost" onClick={closeSignup}>
-              Cancel
-            </Button>
-          </ModalFooter>
+          <form onSubmit={signupFormik.handleSubmit}>
+            <ModalBody pb={6}>
+              <FormControl mt={4}>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  placeholder="Username"
+                  name="username"
+                  onChange={signupFormik.handleChange}
+                  value={signupFormik.values.username}
+                />
+              </FormControl>
+              <FormControl mt={4}>
+                <FormLabel>Password</FormLabel>
+                <Input
+                  placeholder="Password"
+                  type="password"
+                  name="password"
+                  onChange={signupFormik.handleChange}
+                  value={signupFormik.values.password}
+                />
+              </FormControl>
+            </ModalBody>
+            <ModalFooter>
+              <Button colorScheme="blue" mr={3} type="submit">
+                Signup
+              </Button>
+              <Button variant="ghost" onClick={closeSignup}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </form>
         </ModalContent>
       </Modal>
     </Box>
